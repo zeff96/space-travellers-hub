eb0383568c8494e324680a0cf988a81f846cd797
zeff96 <zeffo1975@gmail.com>
2023-04-10 13:11:49 +0300

    initialize store.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import store from "./store/store";
 import { Provider } from "react-redux";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
