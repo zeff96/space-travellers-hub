2f219ea7cdd73979b1bf31cc8321c949822290ee
zeff96 <zeffo1975@gmail.com>
2023-04-10 13:21:15 +0300

    Add fetch rockets data async fucntion.

--- src/store/rocket/rocketSlice.js
+++ src/store/rocket/rocketSlice.js
@@ -1,8 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   rockets: [],
 };
+
+const baseURl = "https://api.spacexdata.com/v4/rockets";
+
+export const getRocketsAsync = createAsyncThunk(
+  "rockets/getRocketsAsyn",
+  async () => {
+    try {
+      const respone = await axios.get(baseURl);
+      return respone.data;
+    } catch (e) {
+      return e.message;
+    }
+  }
+);
 
 export const rocketSlice = createSlice({
   name: "rockets",
