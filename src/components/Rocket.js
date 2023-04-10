259ee3261a05e49101b4bf8fdbdee615cfc07ba9
zeff96 <zeffo1975@gmail.com>
2023-04-10 15:31:22 +0300

    Add reservation button toggle.

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import "./Rocket.scss";
 export default function Rocket({ rocket }) {
+  const [reserve, setReserve] = useState(true);
   return (
     <div className="rocket_wrapper">
       <img className="img" src={rocket.flickr_images[0]} alt={rocket.name} />
       <div>
         <h3 className="name">{rocket.name}</h3>
         <p className="description">{rocket.description}</p>
-        <button className="reserve-btn" type="button">
-          Reserve Rocket
+        <button
+          className={reserve ? "reserve-btn" : "cancel-reserve-btn"}
+          type="button"
+          onClick={() => {
+            setReserve(!reserve);
+          }}
+        >
+          {reserve ? "Reserve Rocket" : "Cancel Reservation"}
         </button>
       </div>
     </div>
