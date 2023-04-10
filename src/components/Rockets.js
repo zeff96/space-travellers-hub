ff6d5617ea64cff8cfe85f48ce67835c87c0392e
zeff96 <zeffo1975@gmail.com>
2023-04-10 13:27:25 +0300

    Add useeffect hook to render fetched rockets when the page is loaded.

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,6 +1,17 @@
 import Rocket from "./Rocket";
+import { useSelector, useDispatch } from "react-redux";
+import { getRocketsAsync, selectedRockets } from "../store/rocket/rocketSlice";
+import { useEffect } from "react";
 
 export default function Rockets() {
+  const rockets = useSelector(selectedRockets);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRocketsAsync());
+  }, [dispatch]);
+
   const listRockets = rockets.map((rocket) => (
     <li key={rocket.id}>
       <Rocket rocket={rocket} />
