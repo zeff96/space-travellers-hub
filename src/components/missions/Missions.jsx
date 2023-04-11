11f30e92023e7a1857e7bc543544ca54ecba5bd3
zeff96 <zeffo1975@gmail.com>
2023-04-11 17:23:01 +0300

    Add font-size and text color for logo text

--- src/components/missions/Missions.jsx
+++ src/components/missions/Missions.jsx
@@ -1,7 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
-import { fetchMissions, missionStatus } from "../../store/missions/missionSlice";
+import {
+  fetchMissions,
+  missionStatus,
+} from "../../store/missions/missionSlice";
 import { useEffect } from "react";
-import './missions.scss';
+import "./missions.scss";
 
 const Missions = () => {
   const { missions, isLoading, error } = useSelector((state) => state.missions);
@@ -10,43 +13,50 @@ const Missions = () => {
     dispatch(fetchMissions());
   }, [dispatch]);
   return (
-    <div className="container-fluid d-none d-md-block">
-      {isLoading && (<h1>Loading Missions..</h1>)}
-      {error && (<h1>{error}</h1>) }
+    <div className="container-fluid">
+      {isLoading && <h1>Loading Missions..</h1>}
+      {error && <h1>{error}</h1>}
       <table className="table table-bordered table-striped container border mt-5 mb-5">
         <tbody>
           <tr>
             <th>Mission</th>
             <th>Description</th>
             <th>Status</th>
-            <th className="text-white">{' '}</th>
+            <th className="text-white"> </th>
           </tr>
           {missions.map((mission) => (
             <tr key={mission.id}>
               <td className="col-md-1">{mission.name}</td>
               <td className="col-md-4 description">{mission.description}</td>
               <td className="align-middle col-md-1 text-center">
-                <span className={mission.joined ? 'member' : 'not-member '}>
-                  {mission.joined ? 'Active Member' : 'NOT A MEMBER'}
-                  {' '}
+                <span className={mission.joined ? "member" : "not-member "}>
+                  {mission.joined ? "Active Member" : "NOT A MEMBER"}{" "}
                 </span>
               </td>
-              <td className={`align-middle col-md-1 text-center ${mission.joined && 'btn-active'}`}>
-                {' '}
+              <td
+                className={`align-middle col-md-1 text-center ${
+                  mission.joined && "btn-active"
+                }`}
+              >
+                {" "}
                 <button
                   type="button"
-                  className={`btn ${mission.joined ? 'btn-outline-danger' : 'btn-outline-secondary'}`}
+                  className={`btn ${
+                    mission.joined
+                      ? "btn-outline-danger"
+                      : "btn-outline-secondary"
+                  }`}
                   onClick={() => dispatch(missionStatus(mission))}
                 >
-                  {mission.joined ? 'Leave Mission' : 'Join Mission'}
+                  {mission.joined ? "Leave Mission" : "Join Mission"}
                 </button>
               </td>
-          </tr>
+            </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
-export default Missions
+export default Missions;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,20 +13,22 @@ const Navbar = () => {
     <header>
       <nav className="d-flex justify-content-between align-items-center px-4 pt-2 mb-4 border-bottom">
         <div>
-          <Link to="/">
-            <h1 className="text-dark">
-              <img src={logo} alt="Logo" style={{ width: "2em" }} /> Space
-              Travelers&apos; Hub
-            </h1>
+          <Link to="/" className="navbar-brand">
+            <img src={logo} alt="Logo" style={{ width: "2em" }} />
+            <span className="text-dark fs-2 ms-2">
+              Space Travelers&apos; Hub
+            </span>
           </Link>
         </div>
-        <ul className="d-flex list">
-          {links.map((link) => (
-            <li key={link.id} className="px-3 list-item">
-              <NavLink to={link.path}>{link.text}</NavLink>
-            </li>
-          ))}
-        </ul>
+        <div>
+          <ul className="d-flex list">
+            {links.map((link) => (
+              <li key={link.id} className="px-3 list-item">
+                <NavLink to={link.path}>{link.text}</NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
       </nav>
     </header>
   );
