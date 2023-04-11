935bcce82204e323e520412ce3b42375473ca5b8
c00p75_ <georgecoopmsapenda@gmail.com>
2023-04-11 07:47:24 +0200

    Create website header with nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.scss";
 import Rockets from "./components/Rockets";
+import MyProfile from "./components/myProfile/MyProfile";
+import Navbar from "./components/navbar/Navbar";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Rockets />
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Rockets />} />
+        <Route exact path="/myProfile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/myProfile/MyProfile.jsx
@@ -0,0 +1,17 @@
+import './myProfile.scss';
+
+const MyProfile = () => {
+  return (
+    <div className="container row">
+      <div className="col-6">
+        <h1>My Missions</h1>
+      </div>
+
+      <div className="col-6">
+        <h1>My Rockets</h1>
+      </div>
+    </div>
+  )
+}
+
+export default MyProfile
